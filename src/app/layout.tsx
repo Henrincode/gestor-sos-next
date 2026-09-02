38972679c2fc9cf3eea5a0dfda1afc878f351360
Henrincode <spellctro@gmail.com>
2026-09-02 01:44:05 -0300

    chore: install framework nextJS

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+})
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Gestor SOS",
+  description: "Sistema Ordem Serviço",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html
+      lang="pt-BR"
+      className={`${inter.variable} ${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col font-inter">{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="
+      flex-1 flex flex-col gap-2 justify-center items-center
+      text-6xl font-bold
+      text-slate-300  bg-slate-800
+    ">
+      <img src="favicon.ico" alt="" />
+      <p>Gestor SOS</p>
+    </div>
+  );
+}
